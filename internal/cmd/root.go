a64a52b07eeb91d1fe2b10990ca282755201b899
Priit Pärkson <priit@parkson.ee>
2020-02-09 12:53:11 +0200

    Init

---
+++ internal/cmd/root.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "fmt"
+
+func WatcherCmd() {
+	fmt.Println("Hello")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/pr11t/go-github-release-watcher/internal/cmd"
+)
+
+func main() {
+	cmd.WatcherCmd()
+}
